e80d8f8f56df9ed7e7c5280c9d0b3f11f65db34b
Kelly <kellyrob@gmail.com>
2021-04-15 11:24:19 -0700

    INT-4473 - Create homebrew package for native image binary - simple starter cask

---
+++ Casks/nexus-iq-cli.rb
@@ -0,0 +1,13 @@
+cask "nexus-iq-cli" do
+  version "1.111.0-01"
+  sha256 "816e8404b9d50b277fedea9ce84e7ca9df6c54ca618889faebca098f4d6a4ad2"
+  url "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.111.0-01+183-mac.pkg"
+  name "Nexus Native IQ CLI"
+  desc "Command line utility for application scanning with Nexus IQ"
+  homepage "https://help.sonatype.com/iqserver"
+  depends_on macos: ">= :mojave"
+  pkg "nexus-iq-cli-1.111.0-01 183-mac.pkg"
+  uninstall pkgutil: [
+    "com.sonatype.nexus.iq.cli"
+  ]
+end
