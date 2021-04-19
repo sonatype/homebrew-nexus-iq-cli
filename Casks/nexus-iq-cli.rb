8f0cf2f20b1a4a40441ea5c1787eed576fbe5290
Kelly <kellyrob@gmail.com>
2021-04-19 09:13:38 -0700

    INT-4473 - Create homebrew package for native image binary - simple starter cask and associated README (as well as license/header)

--- Casks/nexus-iq-cli.rb
+++ Casks/nexus-iq-cli.rb
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2021-present Sonatype, Inc. All rights reserved.
+# Includes the third-party code listed at http://links.sonatype.com/products/nexus/attributions.
+# "Sonatype" is a trademark of Sonatype, Inc.
+#
+
 cask "nexus-iq-cli" do
   version "1.111.0-01"
   sha256 "816e8404b9d50b277fedea9ce84e7ca9df6c54ca618889faebca098f4d6a4ad2"
