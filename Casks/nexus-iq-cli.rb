3cda7efbe58c07a49969fd5084512cab7ba258fe
Eduard Tita <etita@sonatype.com>
2022-06-16 15:20:32 -0400

    IQ 139

--- Casks/nexus-iq-cli.rb
+++ Casks/nexus-iq-cli.rb
@@ -5,14 +5,14 @@
 #
 
 cask "nexus-iq-cli" do
-  version "1.138.0-01"
-  sha256 "f02d291576205636d8738491aa31757daabd1297e78d09971e3701c3da021611"
-  url "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.138.0-01+473-mac.pkg"
+  version "1.139.0-01"
+  sha256 "3fbc88f6a4ebace7e548a6da1d068a557587faa3fba40d8b10ffa145a8710b82"
+  url "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.139.0-01+479-mac.pkg"
   name "Nexus Native IQ CLI"
   desc "Command line utility for application scanning with Nexus IQ"
   homepage "https://help.sonatype.com/integrations/nexus-iq-cli"
   depends_on macos: ">= :mojave"
-  pkg "nexus-iq-cli-1.138.0-01 473-mac.pkg"
+  pkg "nexus-iq-cli-1.139.0-01 479-mac.pkg"
   uninstall pkgutil: [
     "com.sonatype.nexus.iq.cli"
   ]
