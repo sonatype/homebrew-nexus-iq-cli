2777cd43b3daf89e25b26140e6e29f37b3c8da19
Sonatype Zion <zion@sonatype.com>
2025-12-05 14:09:06 +0000

    Updating nexus-iq-cli.rb for IQ 2.8.2-01

--- Casks/nexus-iq-cli.rb
+++ Casks/nexus-iq-cli.rb
@@ -8,9 +8,9 @@ cask "nexus-iq-cli" do
   arch arm: "aarch64", intel: "x86_64"
   pkg_name = on_arch_conditional arm: "aarch_64", intel: "x86_64"
 
-  version "2.8.1-01"
-  sha256 arm:   "5716d1729acbc03afc89728067a04971b6fec93ad66a2fdd7a7e1f68239b0acc",
-         intel: "30522618d6ce738a53394acac06f1b38b607d85dcf457fe4ad5cf9254c1dc5aa"
+  version "2.8.2-01"
+  sha256 arm:   "51f2d783ac90aa3df0ccd77ad55a0c99d8901ca56aabd6e91ab4c6c568fb3f54",
+         intel: "ddc029fbbed41a23afb3fd3d9ac873805fea771902d64c378274c3c086f1a4f8"
 
   url "https://download.sonatype.com/clm/scanner/nexus-iq-cli-#{version}-osx-#{pkg_name}.pkg"
 
